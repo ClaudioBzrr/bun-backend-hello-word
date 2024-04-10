272c18cce77ae53ce7e06675cf809a8b4bcbff07
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-04-10 17:00:28 -0300

    ✔️ Added: configs for api documentation

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,13 @@ import { route } from './routes';
 const port = process.env.SERVER_PORT || 3001;
 const server = new Elysia();
 server.use(route);
-server.use(swagger({ path: '/api-docs' }));
+server.use(
+  swagger({
+    path: '/api-docs',
+    documentation: { info: { title: 'Api Documentation', version: '1.0.0' } },
+    autoDarkMode: true,
+  }),
+);
 
 server.listen(port, () =>
   console.log(`Server running in ${server.server?.url}`),
